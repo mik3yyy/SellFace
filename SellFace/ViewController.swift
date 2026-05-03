b3a31b614b735ee8bb4da91385eac504fe04f6eb
mik3yyy <mikeokpechi@gmail.com>
2026-05-03 09:32:36 +0100

    Initial Commit

---
+++ SellFace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SellFace
+//
+//  Created by okpechi chibuikem michael on 03/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
